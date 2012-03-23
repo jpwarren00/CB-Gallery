90dc0fd297a24bce52d5e3ac7d17f15a38fc4280
Dean Shelton at home <deanshelton913@gmail.com>
2012-03-23 00:34:54 -0400

    sleep..

--- sites/all/modules/custom/cb_gallery/theme/cb_gallery_block.tpl.php
+++ sites/all/modules/custom/cb_gallery/theme/cb_gallery_block.tpl.php
@@ -6,34 +6,66 @@
  * $node->cb_gallery : This contains the cb_gallery data
  *                     for the node being viewed.
  */
- 
+// match the node tpl file. No rewriting for node-cb_gallery.tpl.php
+$cb_gallery = $node->cb_gallery;
+
+// Video player default settings.
+$default_video_height     = 480;
+$default_video_width      = 640;
+$default_video_src        = NULL;
+$shadow_box_height_width  = '';
+
 // Begin by wrapping everything in a div.
 $render='<div class="cb_gallery_node">';
+
 // If we are using viewport, include it here.
-if($node->cb_gallery['gallery_info']['viewport_shadowbox'] === 'viewport') {
+if($cb_gallery['gallery_info']['viewport_shadowbox'] === 'viewport') {
   $render .= '<div class="viewport">viewport</div>';
 }
 // First we check if media exists at all.
-if(isset($node->cb_gallery['media'])) {
+if(isset($cb_gallery['media'])) {
   
   $render .= '<ul class="thumbport clearfix">';
   
   // Then we loop through the images within the gallery.
-  foreach($node->cb_gallery['media'] as $media) {
+  foreach($cb_gallery['media'] as $media) {
     
+    // Extract vars for easy writing. This registers all {cb_children} data.
+    extract($media, EXTR_OVERWRITE);
+    
+    // If this is a video, render it's iframe to a hidden div on the page.
+    if($media_type == 'video') {
+      
+      if(preg_match('/^<iframe/i',$media_content)) {
+        if(preg_match('/(vimeo|youtube)/i',$media_content, $embed_host_matches)) {
+          $embed_host = $embed_host_matches[1];
+        }
+        $vid_height = (preg_match('/height="([0-9 ]*?)"/i',$media_content,$h) ? $h[1] : $default_video_height);
+        $vid_width = (preg_match('/width="([0-9 ]*?)"/i',$media_content,$w) ? $w[1] : $default_video_width);
+        $vid_path = (preg_match('/src="(.*?)"/i',stripslashes($media_content),$s) ? $s[1] : $default_video_src);
+      }else{
+        $embed_host = 'none';
+        $vid_height = (preg_match('/height="([0-9 ]*?)"/i',$media_content,$h) ? $h[1] : $default_video_height);
+        $vid_width = (preg_match('/width="([0-9 ]*?)"/i',$media_content,$w) ? $w[1] : $default_video_width);
+        $vid_path = filter_var($media_content, FILTER_SANITIZE_URL);
+      }
+      
+      $media_content = '/' . drupal_get_path('module', 'cb_gallery') . '/include/player.php?embed_host='.$embed_host.'&src=' . $vid_path . '&thumbnail=' . $media['thumbnail_path'] . '&height=' . $vid_height . '&width=' . $vid_width;
+      $shadow_box_height_width = 'height=' . $vid_height . ';width=' . $vid_width ;
+    }
     $render .= '<li>';
-    $render .= '<p style="height:'.$node->cb_gallery['gallery_info']['thumbnail_height'].'px">';
+    $render .= '<p style="height:' . $cb_gallery['gallery_info']['thumbnail_height'] . 'px">';
     
     // If we are using a shadowbox, we need to add the link here.
-    if($node->cb_gallery['gallery_info']['viewport_shadowbox'] == 'shadowbox'){
-      $render .= '<a href="'.$media['media_content'].'" rel="shadowbox['.$node->nid.'];" title="'.$media['media_caption'].'" onclick="return false;">';
+    if($cb_gallery['gallery_info']['viewport_shadowbox'] == 'shadowbox'){
+      $render .= '<a href="' . $media_content . '" rel="shadowbox[' . $node->nid . '];'.$shadow_box_height_width.'" title="' . $media_caption . '" onclick="return false;">';
     }
     
     // Add the thumbnail image.
-    $render .= '<img src="'.$media['thumbnail_path'].'" style="width:'.$node->cb_gallery['gallery_info']['thumbnail_width'].'px"/>';
+    $render .= '<img src="' . $media['thumbnail_path'] . '" style="width:' . $cb_gallery['gallery_info']['thumbnail_width'] . 'px"/>';
     
     // Close the <a> tag if we are using a shadowbox.
-    if($node->cb_gallery['gallery_info']['viewport_shadowbox'] == 'shadowbox') {
+    if($cb_gallery['gallery_info']['viewport_shadowbox'] == 'shadowbox') {
       $render .= '</a>';
     }
     
